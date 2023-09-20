1508a17e4fd7552f1c6474c76b72e6c410964e52
roroturgeon <118689456+roroturgeon@users.noreply.github.com>
2023-09-19 23:36:16 -0400

    TP1Cro

--- TP1B_main.py
+++ TP1B_main.py
@@ -7,14 +7,14 @@ Created on Tue Sep 12 16:22:09 2023
 import numpy as np
 import math
 from TP1B import parametres_de_vol
-Hp=40000
-T_C=-50
-delISA=False
+Hp=36090
+T_C=0
+delISA=True
 W=40000
-M=0.74
+Vc=275
 
 
-a_kts, a_fts, M, V_kts, V_fts, Ve_kts, Ve_fts, Vc_kts, Vc_fts, pt, q, qc, Tt_C, Tt_K, mu, RN, CL = parametres_de_vol(Hp, T_C, delISA, W, M=M)
+a_kts, a_fts, M, V_kts, V_fts, Ve_kts, Ve_fts, Vc_kts, Vc_fts, pt, q, qc, Tt_C, Tt_K, mu, RN, CL = parametres_de_vol(Hp, T_C, delISA, W, Vc=Vc)
 print(RN)
 print(CL)
 
