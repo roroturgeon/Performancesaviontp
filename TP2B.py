327ca52586b92db2d6de04093614a33238fa35ed
roroturgeon <118689456+roroturgeon@users.noreply.github.com>
2023-10-03 16:27:40 -0400

    TP2B

---
+++ TP2B.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 16:20:42 2023
+
+@author: Rosalie
+"""
+
+import numpy as np
+from TP1A import atmosphere
+from TP1B import parametres_de_vol
+from TP2A import forces
+
+
---
+++ TP2B_main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 16:22:42 2023
+
+@author: Rosalie
+"""
+
+import numpy as np
+from TP1A import atmosphere
+from TP1B import parametres_de_vol
+from TP2A import forces
+# from TP2B import
