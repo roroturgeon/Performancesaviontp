6ad4f49c2dabf2936daa8c956346acc2e953896f
roroturgeon <118689456+roroturgeon@users.noreply.github.com>
2023-11-14 16:55:50 -0500

    TP4A

---
+++ TP4A.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 16:53:49 2023
+
+@author: Rosalie
+"""
+
---
+++ TP4A_main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 16:54:00 2023
+
+@author: Rosalie
+"""
+
