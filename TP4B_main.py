5e551ea985869a0fff2d9d82c2675243054dacde
vincent1703 <vmoreau1703@gmail.com>
2023-11-21 16:51:29 -0500

    Update TP4B_main.py

--- TP4B_main.py
+++ TP4B_main.py
@@ -15,4 +15,21 @@ Hp = 0          # ft
 delISA = True   # Déviation isa
 T_C = 20        # °C
 
+# CAS 2
+V1VR = 0.80    # V1/VR
+W =  30000      # Poids (lbs)
+Hp = 2000          # ft
+delISA = True   # Déviation isa
+T_C = 0        # °C
+
+# CAS 3
+V1VR = 1.0    # V1/VR
+W =  51000      # Poids (lbs)
+Hp = 10000          # ft
+delISA = True   # Déviation isa
+T_C = 35        # °C
+
+
+
+
 longpiste(V1VR, W, Hp, T_C, delISA)
