0034891c9127fa671b611bd2c35dbe17b98b508b
vincent1703 <vmoreau1703@gmail.com>
2023-11-21 16:49:59 -0500

    update main

--- TP4B_main.py
+++ TP4B_main.py
@@ -8,3 +8,11 @@ Created on Tue Nov 21 13:11:37 2023
 import numpy as np
 from TP4B import longpiste
 
+# CAS 1
+V1VR = 0.90    # V1/VR
+W =  45000      # Poids (lbs)
+Hp = 0          # ft
+delISA = True   # Déviation isa
+T_C = 20        # °C
+
+longpiste(V1VR, W, Hp, T_C, delISA)
